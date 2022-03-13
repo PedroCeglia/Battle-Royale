1de403852252d53eda9cab117cc5517003161fc1
PedroCeglia <ceglia.pedro@gmail.com>
2022-03-13 15:46:56 -0300

    Correcting a Bug

--- Assets/Script/Menus/MenuController.cs
+++ Assets/Script/Menus/MenuController.cs
@@ -6,37 +6,34 @@ using Photon.Pun;
 public class MenuController : MonoBehaviourPunCallbacks
 {
     // Menus
-    [SerializeField] GameObject _menuIntro;
-    [SerializeField] GameObject _menuLobby;
-    [SerializeField] GameObject _menuLoadRoom;
+    [SerializeField] IntroMenu _menuIntro;
+    [SerializeField] LobbyMenu _menuLobby;
+    [SerializeField] LoadRoomMenu _menuLoadRoom;
 
     // Start is called before the first frame update
     void Start()
     {
-        _menuIntro.SetActive(true);
-        _menuLobby.SetActive(false);
-        _menuLoadRoom.SetActive(false);
+        _menuIntro.gameObject.SetActive(true);
+        _menuLobby.gameObject.SetActive(false);
+        _menuLoadRoom.gameObject.SetActive(false);
     }
 
     // Called When The User Connect With The Server
     public override void OnConnectedToMaster()
     {
         // Open Menu Lobby
-        SetMenuActive(_menuLobby);
+        SetMenuActive(_menuLobby.gameObject);
     }
 
     // Set menu Active
     private void SetMenuActive(GameObject _menu)
     {
         // Disactive Menus
-        _menuIntro.SetActive(false);
-        _menuLobby.SetActive(false);
-        _menuLoadRoom.SetActive(false);
+        _menuIntro.gameObject.SetActive(false);
+        _menuLobby.gameObject.SetActive(false);
+        _menuLoadRoom.gameObject.SetActive(false);
 
         // Active Menu Choiced
         _menu.SetActive(true);
     }
-
-
-
 }
