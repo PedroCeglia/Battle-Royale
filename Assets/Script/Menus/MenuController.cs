f836d79fe878af5f981485fda9f181de54da897a
PedroCeglia <ceglia.pedro@gmail.com>
2022-03-13 15:41:03 -0300

    Create SetMenuActive Function And Add OnConnectedToMaster

--- Assets/Script/Menus/MenuController.cs
+++ Assets/Script/Menus/MenuController.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class MenuController : MonoBehaviour
+public class MenuController : MonoBehaviourPunCallbacks
 {
+    // Menus
+    [SerializeField] GameObject _menuIntro;
+    [SerializeField] GameObject _menuLobby;
+    [SerializeField] GameObject _menuLoadRoom;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _menuIntro.SetActive(true);
+        _menuLobby.SetActive(false);
+        _menuLoadRoom.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called When The User Connect With The Server
+    public override void OnConnectedToMaster()
     {
-        
+        // Open Menu Lobby
+        SetMenuActive(_menuLobby);
     }
+
+    // Set menu Active
+    private void SetMenuActive(GameObject _menu)
+    {
+        // Disactive Menus
+        _menuIntro.SetActive(false);
+        _menuLobby.SetActive(false);
+        _menuLoadRoom.SetActive(false);
+
+        // Active Menu Choiced
+        _menu.SetActive(true);
+    }
+
+
+
 }
