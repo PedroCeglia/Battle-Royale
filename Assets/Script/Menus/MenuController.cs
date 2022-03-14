600573475219635c189f954695dc70f92898f57b
PedroCeglia <ceglia.pedro@gmail.com>
2022-03-14 17:30:23 -0300

    Create JoinRoom & UserLeaveRoom Function and Set OnJoinedROom

--- Assets/Script/Menus/MenuController.cs
+++ Assets/Script/Menus/MenuController.cs
@@ -37,4 +37,31 @@ public class MenuController : MonoBehaviourPunCallbacks
         // Active Menu Choiced
         _menu.SetActive(true);
     }
+
+    // Called When The User Join In a Room
+    public override void OnJoinedRoom()
+    {
+        SetMenuActive(_menuLoadRoom.gameObject);
+        if(PhotonNetwork.PlayerList.Length == 2)
+        {
+            GestorDeRede.Instance.photonView.RPC("StartTheGame", RpcTarget.All , "GameScene");
+        }
+        else if (PhotonNetwork.PlayerList.Length > 2)
+        {
+            UserLeaveRoom();
+        }
+    }
+
+    // User Leave The Room
+    public void UserLeaveRoom()
+    {
+        GestorDeRede.Instance.UserLeaveRoom();
+        SetMenuActive(_menuLobby.gameObject);
+    }
+
+    // Join A Random Room
+    public void JoinRoom()
+    {
+        GestorDeRede.Instance.JoinRandomRoom();
+    }
 }
