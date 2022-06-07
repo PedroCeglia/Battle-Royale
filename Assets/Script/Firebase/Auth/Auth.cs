0caf929a73d9e138030d89787e0441e900e6fad9
PedroCeglia <ceglia.pedro@gmail.com>
2022-06-07 12:35:20 -0300

    Create Login And Singin Functions

--- Assets/Script/Firebase/Auth/Auth.cs
+++ Assets/Script/Firebase/Auth/Auth.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
 using Firebase.Auth;
 
 public class Auth : MonoBehaviour
 {
+
     // Create Singleton
     public static Auth Instance { get; private set; }
     private void Awake()
@@ -18,7 +20,10 @@ public class Auth : MonoBehaviour
         DontDestroyOnLoad(this);
     }
 
+    // Auth Attributes
+    public FirebaseUser _user { get; private set; }
     private FirebaseAuth _authRef;
+    private bool _isLog;
 
     // Get Auth Reference by Firebase Config
     // Called When Firebase starts
@@ -28,6 +33,155 @@ public class Auth : MonoBehaviour
         _authRef = authRef;
 
         // Create Auth User Listener
+
+    }
+
+    public delegate void GetErroMensage(string erroMensage);
+
+    // User Login Functions
+    public void LoginUser(string email, string password, GetErroMensage SetErroMensage)
+    {
+        object[] parms = new object[3] { email, password, SetErroMensage };
+        StartCoroutine("Login", parms);
+    }
+    IEnumerator Login(object[] parms)
+    {
+        //Call the Firebase auth signin function passing the email and password
+        var LoginTask = _authRef.SignInWithEmailAndPasswordAsync((string)parms[0], (string)parms[1]);
+        //Wait until the task completes
+        yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+
+        if (LoginTask.Exception != null)
+        {
+            //If there are errors handle them
+            //Debug.LogWarning(message: $"Failed to register task with {LoginTask.Exception}");
+            FirebaseException firebaseEx = LoginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+            string message = "Login Failed!";
+            switch (errorCode)
+            {
+                case AuthError.MissingEmail:
+                    message = "Missing Email";
+                    break;
+                case AuthError.MissingPassword:
+                    message = "Missing Password";
+                    break;
+                case AuthError.WrongPassword:
+                    message = "Wrong Password";
+                    break;
+                case AuthError.InvalidEmail:
+                    message = "Invalid Email";
+                    break;
+                case AuthError.UserNotFound:
+                    message = "Account does not exist";
+                    break;
+            }
+            GetErroMensage GetErroLoginMensage = (GetErroMensage)parms[2];
+            GetErroLoginMensage(message);
+        }
+        else
+        {
+            //User is now logged in
+            //Now get the result
+            _user = LoginTask.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})", _user.DisplayName, _user.Email);
+
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+    // Create User Functions
+    public void CreateNewUser(string username, string email, string password, GetErroMensage SetErroMensage)
+    {
+        object[] parms = new object[4] { username, email, password, SetErroMensage };
+        StartCoroutine("CreateUser", parms);
+    }
+    IEnumerator CreateUser(object[] parms)
+    {
+
+        string username = (string)parms[0];
+        string email = (string)parms[1];
+        string password = (string)parms[2];
+        GetErroMensage GetErroLoginMensage = (GetErroMensage)parms[3];
+        if (username != null && email != null && password != null && GetErroLoginMensage != null)
+        {
+            if (username == "")
+            {
+                //If the username field is blank show a warning
+                GetErroLoginMensage("Missing Username");
+            }
+            else if (email.IndexOf("@") < 4 && email.IndexOf(".com") < 6)
+            {
+                //If the password does not match show a warning
+                GetErroLoginMensage("The Password needs more than 5 words");
+            }
+            else
+            {
+                //Call the Firebase auth signin function passing the email and password
+                var RegisterTask = _authRef.CreateUserWithEmailAndPasswordAsync(email, password);
+                //Wait until the task completes
+                yield return new WaitUntil(predicate: () => RegisterTask.IsCompleted);
+
+                if (RegisterTask.Exception != null)
+                {
+                    //If there are errors handle them
+                    //Debug.LogWarning(message: $"Failed to register task with {RegisterTask.Exception}");
+                    FirebaseException firebaseEx = RegisterTask.Exception.GetBaseException() as FirebaseException;
+                    AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+                    string message = "Register Failed!";
+                    switch (errorCode)
+                    {
+                        case AuthError.MissingEmail:
+                            message = "Missing Email";
+                            break;
+                        case AuthError.MissingPassword:
+                            message = "Missing Password";
+                            break;
+                        case AuthError.WeakPassword:
+                            message = "Weak Password";
+                            break;
+                        case AuthError.EmailAlreadyInUse:
+                            message = "Email Already In Use";
+                            break;
+                    }
+                    GetErroLoginMensage(message);
+                }
+                else
+                {
+                    //User has now been created
+                    //Now get the result
+                    _user = RegisterTask.Result;
+
+                    if (_user != null)
+                    {
+
+                        // Create User In Database
+                        //UserModel userDatabase = new UserModel(username, email, _user.UserId);
+                        //_databaseAPI.WriteNewPlayerDatabase(userDatabase);
+
+                        //Create a user profile and set the username
+                        UserProfile profile = new UserProfile { DisplayName = username };
+
+                        //Call the Firebase auth update user profile function passing the profile with the username
+                        var ProfileTask = _user.UpdateUserProfileAsync(profile);
+                        //Wait until the task completes
+                        yield return new WaitUntil(predicate: () => ProfileTask.IsCompleted);
+
+                        if (ProfileTask.Exception != null)
+                        {
+                            //If there are errors handle them
+                            Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
+                        }
+                        else
+                        {
+                            //Username is now set
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
--- Assets/Script/Menus/LogMenu.cs
+++ Assets/Script/Menus/LogMenu.cs
@@ -11,15 +11,20 @@ public class LogMenu : MonoBehaviour
     [SerializeField] private InputField _loginEmail;
     [SerializeField] private InputField _loginPassword;
     [SerializeField] private GameObject _loginErroArea;
-    [SerializeField] private Text _loginErroMensage;
+    [SerializeField] private Text _loginErroMensageField;
     [Header("Singin Attributes")]
     [SerializeField] private GameObject _menuSingin;
     [SerializeField] private InputField _singinName;
     [SerializeField] private InputField _singinEmail;
     [SerializeField] private InputField _singinPassword;
     [SerializeField] private GameObject _singinErroArea;
-    [SerializeField] private Text _singinErroMensage;
+    [SerializeField] private Text _singinErroMensageField;
 
+    // Atributes
+    private bool _hasSinginErroMensage;
+    private string _singinErroMensage;
+    private bool _hasLoginErroMensage;
+    private string _loginErroMensage;
 
     // Set Menu Log
     public void SetMenulog(string menu)
@@ -61,5 +66,71 @@ public class LogMenu : MonoBehaviour
     {
         _loginErroMensage.text = "";
         _loginErroArea.SetActive(false);
+    }
+
+    // Singin Functions
+    public void SinginUser()
+    {
+        if(_singinName.text.Length > 4)
+        {
+            if(_singinEmail.text.IndexOf("@") > 4 && _singinEmail.text.IndexOf(".com") > 7)
+            {
+                if(_singinPassword.text.Length > 6)
+                {
+                    // Create User
+
+                    // Set Singin Field
+                    ClearSinginFields();
+
+                }
+                else
+                {
+                    GetSinginMensageErro("The user password needs more than 6 letters");
+                }
+            }
+            else
+            {
+                GetSinginMensageErro("Write a Valid Email ex: xxxx@gmail.com");
+            }
+        }
+        else
+        {
+            GetSinginMensageErro("The user name needs more than 4 letters");
+        }
+    }
+    // Get Singin ErroMensage
+    private void GetSinginMensageErro(string erroMensage)
+    {
+        _hasSinginErroMensage = true;
+        _singinErroMensage = erroMensage;
+    }
+
+    // Login Function
+    public void LoginUser()
+    {
+        if (_loginEmail.text.IndexOf("@") > 4 && _loginEmail.text.IndexOf(".com") > 7)
+        {
+            if (_loginPassword.text.Length > 6)
+            {
+                // Log User
+
+                // Set Login Field
+                ClearLoginFields();
+            }
+            else
+            {
+                GetLoginMensageErro("The user password needs more than 6 letters");
+            }
+        }
+        else
+        {
+            GetLoginMensageErro("Write a Valid Email ex: xxxx@gmail.com");
+        }
+    }
+    // Get Login ErroMensage
+    private void GetLoginMensageErro(string erroMensage)
+    {
+        _hasLoginErroMensage = true;
+        _loginErroMensage = erroMensage;
     }
 }
