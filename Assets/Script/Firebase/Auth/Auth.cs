48c4c68a2c307a22bb5f1cab0c1d6ab5d6bf3569
PedroCeglia <ceglia.pedro@gmail.com>
2022-06-07 15:41:56 -0300

    Create SetUserName and SetUserEmail Functions

--- Assets/Script/Firebase/Auth/Auth.cs
+++ Assets/Script/Firebase/Auth/Auth.cs
@@ -216,7 +216,77 @@ public class Auth : MonoBehaviour
             }
         }
     }
-    
+
+    // Set User Name 
+    public void SetUserName(string username)
+    {
+        if (username.Length > 4)
+        {
+            StartCoroutine("SetUserNameCorotine", username);
+        }
+    }
+    IEnumerator SetUserNameCorotine(string username)
+    {
+        if (_user != null)
+        {
+            //Create a user profile and set the username
+            UserProfile profile = new UserProfile { DisplayName = username };
+
+            //Call the Firebase auth update user profile function passing the profile with the username
+            var ProfileTask = _user.UpdateUserProfileAsync(profile);
+            //Wait until the task completes
+            yield return new WaitUntil(predicate: () => ProfileTask.IsCompleted);
+
+            if (ProfileTask.Exception != null)
+            {
+                //If there are errors handle them
+                Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
+
+            }
+            else
+            {
+                //Username is now set
+
+                // Set In Database
+                RealtimeDatabase.Instance.SetUserNameInDatabase(_user.UserId, username);
+            }
+        }
+    }
+
+    // Set User Email
+    public void SetUserEmail(string email)
+    {
+        if (email.IndexOf("@") < 4 && email.IndexOf(".com") < 6)
+        {
+            StartCoroutine("SetUserEmailCorotine", email);
+        }
+    }
+    IEnumerator SetUserEmailCorotine(string email)
+    {
+        if (_user != null)
+        {
+            var task = _user.UpdateEmailAsync(email);
+
+            yield return new WaitUntil(predicate: () => task.IsCompleted);
+
+            if (task.IsCanceled)
+            {
+                // UpdateEmailAsync was canceled.
+
+            }
+            else if (task.IsFaulted)
+            {
+                // UpdateEmailAsync encountered an error:  task.Exception
+            }
+            else
+            {
+                // User email updated successfully.
+                // Set User Email In Database
+                RealtimeDatabase.Instance.SetUserEmailInDatabase(_user.UserId, email);
+            }
+        }
+    }
+
 
     // Called before the Component Destroy
     void OnDestroy()
