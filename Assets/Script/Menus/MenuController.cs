b0039e5c5b5cbab06265892080ca1b9dfc89abc9
PedroCeglia <ceglia.pedro@gmail.com>
2022-06-13 11:39:12 -0300

    Create a Listener to Change All Menus

--- Assets/Script/Menus/MenuController.cs
+++ Assets/Script/Menus/MenuController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class MenuController : MonoBehaviourPunCallbacks
 {
@@ -9,41 +10,109 @@ public class MenuController : MonoBehaviourPunCallbacks
     [SerializeField] IntroMenu _menuIntro;
     [SerializeField] LobbyMenu _menuLobby;
     [SerializeField] LoadRoomMenu _menuLoadRoom;
+    [SerializeField] LogMenu _menuLog;
 
-    // Start is called before the first frame update
+    // Atributes
+    private bool _isIntroRoom;
+    private bool _isLogRoom;
+    private bool _isLobbyRoom;
+    private bool _isLoadRoom;
+    private bool _isServerConnect;
+
+    // Set The Menu to MenuLoadRoom
     void Start()
     {
-        _menuIntro.gameObject.SetActive(true);
-        _menuLobby.gameObject.SetActive(false);
-        _menuLoadRoom.gameObject.SetActive(false);
+        SetMenuActive("intro");
+    }
+
+    // Manager Menus
+    private void Update()
+    {
+        // If Intro Menu Is Active
+        if (_isIntroRoom)
+        {
+            // If Firebase Is Connect And Photon Is Connect
+            if(_isServerConnect && FirebaseConfig.Instance._isConnect)
+            {
+                _isIntroRoom = false;
+            }
+        }
+        else
+        {
+            // If User Is Log
+            if(Auth.Instance._user != null)
+            {
+                // If Lobby Menu Is Active
+                if (!_isLobbyRoom && !_isLoadRoom)
+                {
+                    SetMenuActive("lobby");
+                }
+            }
+            else
+            {
+                // If Log Menu Is Active
+                if (!_isLogRoom)
+                {
+                    SetMenuActive("log");
+                }
+            }
+        }
+
     }
 
     // Called When The User Connect With The Server
     public override void OnConnectedToMaster()
     {
-        // Open Menu Lobby
-        SetMenuActive(_menuLobby.gameObject);
+        _isServerConnect = true;
     }
 
     // Set menu Active
-    private void SetMenuActive(GameObject _menu)
+    private void SetMenuActive(string menu)
     {
         // Disactive Menus
         _menuIntro.gameObject.SetActive(false);
         _menuLobby.gameObject.SetActive(false);
         _menuLoadRoom.gameObject.SetActive(false);
+        _menuLog.gameObject.SetActive(false);
+
+        // Reset Bool Menu Attributes
+        _isIntroRoom = false;
+        _isLobbyRoom = false;
+        _isLoadRoom = false;
+        _isLogRoom = false;
 
         // Active Menu Choiced
-        _menu.SetActive(true);
+        switch (menu)
+        {
+            case "intro":
+                _menuIntro.gameObject.SetActive(true);
+                _isIntroRoom = true;
+                break;
+            case "lobby":
+                _menuLobby.gameObject.SetActive(true);
+                _isLobbyRoom = true;
+                break;
+            case "load":
+                _menuLoadRoom.gameObject.SetActive(true);
+                _isLoadRoom = true;
+                break;
+            case "log":
+                _menuLog.gameObject.SetActive(true);
+                _isLogRoom = true;
+                break;
+        }
     }
 
     // Called When The User Join In a Room
     public override void OnJoinedRoom()
     {
-        SetMenuActive(_menuLoadRoom.gameObject);
+        SetMenuActive("load");
         Debug.Log(PhotonNetwork.PlayerList.Length);
         if (PhotonNetwork.PlayerList.Length == 2)
         {
+            // Create Room In Firebase
+
+            // Create Room In Photon
             GestorDeRede.Instance.photonView.RPC("StartTheGame", RpcTarget.All);
         }
         else if (PhotonNetwork.PlayerList.Length > 2)
@@ -52,13 +121,11 @@ public class MenuController : MonoBehaviourPunCallbacks
         }
     }
 
-
-
     // User Leave The Room
     public void UserLeaveRoom()
     {
         GestorDeRede.Instance.UserLeaveRoom();
-        SetMenuActive(_menuLobby.gameObject);
+        SetMenuActive("lobby");
     }
 
     // Join A Random Room
